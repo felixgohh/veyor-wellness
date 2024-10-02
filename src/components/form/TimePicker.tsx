f66973d24860279b50203cb83f4a8ec928a88008
Felix <felixgohh08@gmail.com>
2024-10-03 00:47:33 +0700

    add time picker component

---
+++ src/components/form/TimePicker.tsx
@@ -0,0 +1,39 @@
+import { Controller } from 'react-hook-form';
+
+export default function TimePicker({
+  timeSlots,
+  name,
+}: {
+  timeSlots: string[];
+  name: string;
+}) {
+  return (
+    <Controller
+      name={name}
+      render={({ field: { onChange, value } }) => {
+        return (
+          <div className="flex flex-col mt-5">
+            <h2 className="font-bold mb-4">Please select a time</h2>
+            {timeSlots.map((slot, index) => (
+              <div key={index} className="mb-4">
+                <label className="flex items-center cursor-pointer">
+                  <input
+                    type="radio"
+                    name="time"
+                    value={slot}
+                    checked={value === slot}
+                    onChange={(ev: React.ChangeEvent<HTMLInputElement>) =>
+                      onChange(ev.target.value)
+                    }
+                    className="h-5 w-5 accent-black"
+                  />
+                  <span className="ml-2 text-sm md:text-base">{slot}</span>
+                </label>
+              </div>
+            ))}
+          </div>
+        );
+      }}
+    ></Controller>
+  );
+}
