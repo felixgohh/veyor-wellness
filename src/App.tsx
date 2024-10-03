53c5bc89c86fad4729f2bbbe6f263d8ab7f85dfe
Felix <felixgohh08@gmail.com>
2024-10-03 14:06:08 +0700

    change App flow with Outlet from react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { Suspense } from 'react';
 import MainLayout from './components/layouts/MainLayout';
-import { bookingForm } from './utils/validation';
-import Form from './components/form';
-import { BookingFormType } from './shared/booking.type';
-import Appointment from './sections/Appointment';
-import { useSection } from './context/SectionContext';
-import GuestInfo from './sections/GuestInfo';
-import Button from './components/Button';
+import { Outlet } from 'react-router-dom';
+import Loading from './components/Loading';
 
 function App() {
-  const methods = useForm<BookingFormType>({
-    resolver: yupResolver(bookingForm),
-  });
-  const { activeSection, nextSection } = useSection();
-  const onSubmit: SubmitHandler<BookingFormType> = (data) => nextSection();
-
   return (
     <MainLayout>
-      <Form methods={methods} onSubmit={onSubmit}>
-        {activeSection === 0 ? (
-          <Appointment />
-        ) : activeSection === 1 ? (
-          <GuestInfo />
-        ) : null}
-
-        {/* shows submit button if user has reach 'Your Info' section */}
-        {activeSection === 1 ? (
-          <Button title="Compelete Appointment" type="submit" />
-        ) : null}
-      </Form>
+      <Suspense fallback={<Loading />}>
+        <Outlet />
+      </Suspense>
     </MainLayout>
   );
 }
