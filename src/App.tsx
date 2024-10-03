e680744df96f7545e0685d941028c49ac34d4b8b
Felix <felixgohh08@gmail.com>
2024-10-04 00:47:05 +0700

    update import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import MainLayout from './components/layouts/MainLayout';
+import MainLayout from './components/layout/MainLayout';
 import { Outlet } from 'react-router-dom';
 import Loading from './components/Loading';
 
