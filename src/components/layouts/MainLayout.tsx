b1dcf11e6f6c063c8246ab8f64cc78b1d62b57b9
Felix <felixgohh08@gmail.com>
2024-10-04 00:27:14 +0700

    update section list design

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSection } from '../../context/SectionContext';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { useBooking } from '../../context/BookingContext';
 import { formatDate } from '../../utils/time';
 
@@ -11,7 +11,6 @@ export default function MainLayout({
 }) {
   const { sectionList, activeSection, setActiveSection } = useSection();
   const { bookingList } = useBooking();
-  const navigate = useNavigate();
   const location = useLocation();
 
   useEffect(() => {
@@ -33,24 +32,29 @@ export default function MainLayout({
         </p>
       </header>
       <main className="w-full">
-        <div className="flex flex-row w-full border border-gray-400 mb-10">
-          {sectionList.map((section) => (
-            <button
-              type="button"
+        <ul className="flex flex-row border border-gray-400 rounded-md mb-10">
+          {sectionList.map((section, index) => (
+            <li
               key={section.path}
-              onClick={() => {
-                if (section.path !== activeSection.path) navigate(section.path);
-              }}
-              className={`text-sm md:text-base p-2 flex-1 font-semibold ${
+              className={`relative first:rounded-tl-md first:rounded-bl-md last:rounded-tr-md last:rounded-br-md text-sm md:text-base p-2 flex-1 font-semibold flex items-center justify-center text-center ${
                 activeSection.path === section.path
                   ? 'bg-white text-black border-b border-b-black'
                   : 'bg-gray-100 text-gray-500'
               }`}
             >
               {section.name}
-            </button>
+              {index !== sectionList.length - 1 ? (
+                <div
+                  className={`absolute z-10 right-[-8px] md:right-[-18px] top-1/2 transform -translate-y-1/2 h-0 md:h-full w-0 border-l-8 md:border-l-[20px] border-transparent border-t-8 md:border-t-[20px] border-b-8 md:border-b-[20px] ${
+                    activeSection.path === section.path
+                      ? 'border-l-white'
+                      : 'border-l-gray-100'
+                  }`}
+                ></div>
+              ) : null}
+            </li>
           ))}
-        </div>
+        </ul>
         {bookingList.length ? (
           <div className="bg-green-200 text-green-800 px-4 py-2 rounded-lg mb-5 text-sm md:text-base">
             <p className="font-semibold">Upcoming appointments</p>
@@ -64,7 +68,7 @@ export default function MainLayout({
             </ul>
           </div>
         ) : null}
-        {children}
+        <div className="md:w-[90%] md:mx-auto">{children}</div>
       </main>
     </div>
   );
