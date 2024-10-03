c5d813393ab299b79f152048f1352c0206c85965
Felix <felixgohh08@gmail.com>
2024-10-03 20:10:31 +0700

    add classname props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,18 +2,20 @@ type ButtonProps = {
   title: string;
   type?: 'button' | 'submit' | 'reset';
   onClick?: () => void;
+  className?: string;
 };
 
 export default function Button({
   title,
   type = 'button',
   onClick,
+  className,
 }: ButtonProps) {
   return (
     <button
       type={type}
       onClick={onClick}
-      className="px-4 py-2 bg-black rounded-lg text-white w-fit"
+      className={`px-4 py-2 bg-black rounded-lg text-white w-fit ${className}`}
     >
       {title}
     </button>
