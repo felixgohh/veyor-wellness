0ae591f0ccb0cae86e994aafe2fb4f950ec86d2f
Felix <felixgohh08@gmail.com>
2024-10-04 15:03:08 +0700

    fix width and height value

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,6 @@
 export default function NotFoundPage() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
+    <div className="flex justify-center items-center w-full h-full">
       <h1 className="text-xl font-bold">404 - Page Not Found</h1>
     </div>
   );
