1ca3dcc82c03b7cfcf91b7e7a2f402f7f50ed715
Felix <42145412+felixgohh@users.noreply.github.com>
2024-10-04 14:50:49 +0700

    bugfix(loading): fix loading spinner style issue (#14)
    * add style classes to show the overlay
    
    * add animation for loading spinner

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        stroke: 'stroke 4s linear infinite',
+      },
+      keyframes: {
+        stroke: {
+          '0%, 100%': {
+            strokeDashoffset: 440,
+          },
+          '50%': {
+            strokeDashoffset: 0,
+          },
+          '50.1%': {
+            strokeDashoffset: 880,
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
