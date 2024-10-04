54587ac6dd36fe02e75703724640c19a25637b7d
Felix <42145412+felixgohh@users.noreply.github.com>
2024-10-04 15:06:41 +0700

    bugfix(not-found-page): fix styling and add redirection link (#15)
    * fix width and height value
    
    * add link to redirect to index page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export default function NotFoundPage() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
-      <h1 className="text-xl font-bold">404 - Page Not Found</h1>
+    <div className="flex flex-col justify-center my-20 items-center w-full h-full">
+      <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
+      <Link to="/" className="px-4 py-2 rounded-lg border border-gray-400 mt-5">
+        Back to Booking
+      </Link>
     </div>
   );
 }
